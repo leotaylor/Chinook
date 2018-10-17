421c4b03fb330cf4e23ec190901aaa06c3e8b204
Leo Taylor <leotaylor723@gmail.com>
2018-10-16 19:30:10 -0500

    query1

---
+++ ChinookQuery1.sql
@@ -0,0 +1,9 @@
+-- non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) 
+-- who are not in the US.
+
+SELECT 
+	FullName = FirstName + ' ' + LastName,
+	CustomerId,
+	Country
+from Customer
+WHERE Country != 'USA'
