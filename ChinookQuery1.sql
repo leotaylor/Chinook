cf21bcfad29edfa1f94184434cad4a0070c24e8d
Leo Taylor <leotaylor723@gmail.com>
2018-10-16 19:57:08 -0500

    query5

--- ChinookQuery1.sql
+++ ChinookQuery1.sql
@@ -33,3 +33,9 @@ SELECT
 	FullName = FirstName + ' ' + LastName
 FROM Employee
 Where Title = 'Sales Support Agent'
+
+
+-- unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
+
+SELECT DISTINCT BillingCountry
+FROM Invoice
